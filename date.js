d435d989bc0227f967218352a382a865fd69d245
reynolds-philipt <reynolds.philipt@gmail.com>
2017-07-22 17:44:06 -0400

    on load

--- date.js
+++ date.js
@@ -2,3 +2,8 @@ function date() {
 	debugger;
 	document.getElementById('header-button-date').style.backgroundColor('#42eef4');
 }
+
+window.onload = function() {
+	debugger;
+	document.getElementById('header-button-date').style.backgroundColor('#42eef4');
+}

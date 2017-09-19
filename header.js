d51dbf49d850c81b46d2834752c6284341799631
reynolds-philipt <reynolds.philipt@gmail.com>
2017-09-18 21:59:52 -0400

    adding database

--- header.js
+++ header.js
@@ -1,6 +1,16 @@
 function header() {
-	debugger;
 	var headerText = document.createElement("div");
+	// Initialize Firebase
+	debugger;
+	var config = {
+	  apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
+	  authDomain: "wedding-360af.firebaseapp.com",
+	  databaseURL: "https://wedding-360af.firebaseio.com",
+	  projectId: "wedding-360af",
+	  storageBucket: "",
+	  messagingSenderId: "833865959590"
+	};
+	firebase.initializeApp(config);
 
 }
 

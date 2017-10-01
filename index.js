58fc8629caf925813b29da6a89f5c7278ac9f269
reynolds-philipt <reynolds.philipt@gmail.com>
2017-09-30 21:36:12 -0400

    set up firebase in index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function index() {
-  
+	debugger;
+	var config = {
+	  apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
+	  authDomain: "wedding-360af.firebaseapp.com",
+	  databaseURL: "https://wedding-360af.firebaseio.com",
+	  projectId: "wedding-360af",
+	  storageBucket: "",
+	  messagingSenderId: "833865959590"
+	};
+	firebase.initializeApp(config);
 }

6c79448147ca1f7000ed196cb39235f5e948e6fc
reynolds-philipt <reynolds.philipt@gmail.com>
2017-10-16 23:18:41 -0400

    testing focus

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function index() {
 	debugger;
+	var self = this;
 	var config = {
 	  apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
 	  authDomain: "wedding-360af.firebaseapp.com",
@@ -10,6 +11,17 @@ function index() {
 	};
 	firebase.initializeApp(config);
 	var database = firebase.database();
+	
+	var user_email = document.getElementById('user_email');
+	var user_first_name = document.getElementById('user_first_name');
+	var user_last_name = document.getElementById('user_last_name');
+	
+	user_email.addEventListener('focus', function() {
+		user_email.style({
+			'opacity':100	
+		});
+	});
+	
 	var user_id = 2;
 	var email = "testing@yahoo.com";
 	var first_name = "Test";

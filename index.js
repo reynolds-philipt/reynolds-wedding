49c28fc4c22b400ead9148ac86265c3b665588b7
reynolds-philipt <reynolds.philipt@gmail.com>
2017-10-27 00:55:03 -0400

    removing name_input

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ function index() {
 function display_names() {
 	var first_name_input = document.getElementById('user_first_name');
 	var last_name_input = document.getElementById('user_last_name');
-	first_name_input[name_id].style.display = 'inline';
-	first_name_input[name_id].style.position = 'relative';
-	last_name_input[name_id].style.display = 'inline';
-	last_name_input[name_id].style.position = 'relative';
+	first_name_input.style.display = 'inline';
+	first_name_input.style.position = 'relative';
+	last_name_input.style.display = 'inline';
+	last_name_input.style.position = 'relative';
 }

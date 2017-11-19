8ecac0728e5a6f1f408accae61379401e38c6a36
reynolds-philipt <reynolds.philipt@gmail.com>
2017-11-18 22:55:13 -0500

    adding user specific logout

--- header.js
+++ header.js
@@ -1,4 +1,5 @@
 function header() {
+	var self = this;
 	var headerText = document.createElement("div");
 	// Initialize Firebase
 	debugger;
@@ -14,5 +15,11 @@ function header() {
 	
 	document.getElementById('date_header_button');
 
+	var header_user_login = document.getElementById('header_user_login_out');
+	
+	var user = self.get('user');
+	if (user) {
+		header_user_login.innerHTML(user.first_name + ' ' + user.last_name + ' (Sign out)';
+	}
 }
 

27329c1ce16ed78ef86c41fe6dd5f709202323a9
reynolds-philipt <reynolds.philipt@gmail.com>
2017-11-19 00:49:22 -0500

    moving functions needed by everyone to js.js

---
+++ js.js
@@ -0,0 +1,43 @@
+
+
+function save(save_string_key, save_string) {
+	var self = this;
+	//converts to JSON string the Object
+	var json_save_string = JSON.stringify(save_string);
+	//creates a base-64 encoded ASCII string
+	var encoded_save_string = btoa(json_save_string);
+	//save the encoded accout to web storage
+	localStorage.setItem('_' + save_string_key, encoded_save_string);
+}
+
+function load(load_string_key) {
+	var self = this;
+	var encoded_load_string = localStorage.getItem('_' + load_string_key);
+	if (!encoded_load_string) return false;
+	//decodes a string data encoded using base-64
+	var decoded_load_string = atob(encoded_load_string);
+	//parses to Object the JSON string
+	var load_string = JSON.parse(decoded_load_string);
+	//do what you need with the Object
+	window[load_string_key] = load_string;
+	return true;
+}
+
+function remove(remove_string_key) {
+	localStorage.removeItem('_' + remove_string_key);
+	return true;
+}
+
+function add_load_event(func) {
+  var old_on_load = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (old_on_load) {
+        old_on_load();
+      }
+      func();
+    }
+  }
+}

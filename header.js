8cb280f01fe21f3c38a3e4b9eb94c7e466b7b546
reynolds-philipt <reynolds.philipt@gmail.com>
2017-11-19 00:34:16 -0500

    remove debug

--- header.js
+++ header.js
@@ -20,7 +20,6 @@ function header() {
 	if (user_saved) {
 		header_user_login.innerHTML = window.user.first_name + ' ' + window.user.last_name + ' (Sign out)';
 		header_user_login.addEventListener('click', function() {
-			debugger;
 			self.remove('user');
 			render();
 		});

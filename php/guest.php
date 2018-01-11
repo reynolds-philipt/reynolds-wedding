d89e2b586f4ca40b38844e56c5054c9690e672ae
reynolds-philipt <reynolds.philipt@gmail.com>
2018-01-11 00:14:03 -0500

    push to firebase

--- php/guest.php
+++ php/guest.php
@@ -1,13 +1,13 @@
 <?PHP
 
-const DEFAULT_URL = '';
-const DEFAULT_TOKEN = '';
-const DEFAULT_PATH = '';
+const DEFAULT_URL = 'https://wedding-360af.firebaseio.com';
+const DEFAULT_TOKEN = 'AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w';
+const DEFAULT_PATH = 'guests'; // 'wedding-360af.firebaseapp.com';
 
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
 public function save_guest($guest_object) {
-  
+  $firebase->push(DEFAULT_PATH, $guest_object);
 }
 
 public function get_guest($guest_object) {

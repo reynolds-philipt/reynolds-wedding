01abe5cb79154e0cae932ec6a925c410f08a9b10
reynolds-philipt <reynolds.philipt@gmail.com>
2018-01-10 23:31:03 -0500

    guest php

---
+++ php/guest.php
@@ -0,0 +1,15 @@
+<?PHP
+
+const DEFAULT_URL = '';
+const DEFAULT_TOKEN = '';
+const DEFAULT_PATH = '';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+public function save_guest($guest_object) {
+  
+}
+
+public function get_guest($guest_object) {
+  
+}

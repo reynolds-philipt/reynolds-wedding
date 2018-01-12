8b5cfd3c23ca222891376f162775629b2526e63f
reynolds-philipt <reynolds.philipt@gmail.com>
2018-01-11 23:03:31 -0500

    updating to tell them not to

--- js.js
+++ js.js
@@ -28,15 +28,17 @@ function remove_local(remove_string_key) {
 }
 
 function get_config() {
-  var config = {
-	  apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
-	  authDomain: "wedding-360af.firebaseapp.com",
-	  databaseURL: "https://wedding-360af.firebaseio.com",
-	  projectId: "wedding-360af",
-	  storageBucket: "",
-	  messagingSenderId: "833865959590"
+	// Please be nice.  Don't mess with my database.
+	// I will fix it soon.
+  	var config = {
+		apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
+		authDomain: "wedding-360af.firebaseapp.com",
+		databaseURL: "https://wedding-360af.firebaseio.com",
+		projectId: "wedding-360af",
+		storageBucket: "",
+		messagingSenderId: "833865959590"
 	};
-  return config;
+	return config;
 }
 
 function add_load_event(func) {

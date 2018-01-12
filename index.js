18442e5e621464d7dc03fa21a77c90348a969de7
reynolds-philipt <reynolds.philipt@gmail.com>
2018-01-11 22:50:17 -0500

    save without php for now

--- index.js
+++ index.js
@@ -62,13 +62,12 @@ function index() {
 			var email = user_email.value;
 			var first_name = first_name_input.value;
 			var last_name = last_name_input.value;
-			/*
 			var new_user = database.ref('guests').push({
 				email: email,
 				first_name: first_name,
 				last_name: last_name
-			});*/
-			var new_user = {
+			});
+			/*var new_user = {
 				email: email,
 				first_name: first_name,
 				last_name: last_name
@@ -80,7 +79,7 @@ function index() {
 				url: url,
 				data: JSON.stringify(new_user),
 				dataType: 'json'
-			});
+			});*/
 			window.user = {
 				'id': new_user.path.ct[1],
 				'email': email,

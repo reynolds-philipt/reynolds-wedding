38255316184bbfd80d40a116dac656f237f782dd
reynolds-philipt <reynolds.philipt@gmail.com>
2018-01-11 23:08:43 -0500

    no debug

--- header.js
+++ header.js
@@ -1,19 +1,5 @@
 function header() {
   var self = this;
-  // Initialize Firebase
-  /*
-  var config = {
-    apiKey: "AIzaSyBAPX4ORn6MWiFyNXUhg-eZ07awM4UeY9w",
-    authDomain: "wedding-360af.firebaseapp.com",
-    databaseURL: "https://wedding-360af.firebaseio.com",
-    projectId: "wedding-360af",
-    storageBucket: "",
-    messagingSenderId: "833865959590"
-  };
-  firebase.initializeApp(config);
-
-  document.getElementById('date_header_button');
-*/
   var header_user_login = document.getElementById('header_user_login_out');
 
   var user_saved = self.load_local('user');
@@ -25,7 +11,6 @@ function header() {
     });
   }
 
-  debugger;
   var pathname = window.location.pathname;
   if (pathname.length > 20) {
     var usable_pathname = pathname.substr(18, pathname.length - 23);

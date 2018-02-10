c708d7aa9389b59e473f831681b30c0d9e7aa8d4
reynolds-philipt <reynolds.philipt@gmail.com>
2018-02-10 16:41:38 -0500

    get guests

--- admin/guest_list.js
+++ admin/guest_list.js
@@ -1,3 +1,13 @@
 function guest_list() {
+	var self = this;
+	var config = self.get_config();
+	firebase.initializeApp(config);
+	var database = firebase.database();
+  
+	var guest_list_table = document.getElementById('admin_guest_list_table');
+  
+  
+  var guests = database.ref("guests").orderByKey();
+  debugger;
 
 }

ea0ef0f3a2941f2ce7d3e1a63445ba3b0a994a4a
reynolds-philipt <reynolds.philipt@gmail.com>
2018-02-13 22:04:07 -0500

    add testing button

--- js/rsvp.js
+++ js/rsvp.js
@@ -5,5 +5,13 @@ function rsvp() {
 	var database = firebase.database();
 
   debugger;
+	var guest_input_div = document.getElementById('guest_input_div');
+	decorate_guest_input_block(guest_input_div, 1);
 
+}
+
+function decorate_guest_input_block(parent, guest_number) {
+	var guest_attending_toggle = document.createElement('BUTTON');
+	guest_attending_toggle.innerHTML = 'Testing';
+	parent.appendChild(guest_attending_toggle);
 }

ff44d542225f4715f9fc87b973e88cf251dcea3c
reynolds-philipt <reynolds.philipt@gmail.com>
2018-02-24 17:00:44 -0500

    higher than 0 selected

--- js/rsvp.js
+++ js/rsvp.js
@@ -24,7 +24,7 @@ function decorate_rsvp_page() {
 function decorate_guest_input_blocks_calculation() {
 	var selected_number_of_guests = window.user.selected_number_of_guests;
 	var guest_input_div = document.getElementById('guest_input_div');
-	if (selected_number_of_guests) {
+	if (selected_number_of_guests && selected_number_of_guests > 0) {
 		for (var i = 0; i < number_of_guests; i++) {
 			decorate_guest_input_block(guest_input_div, i + 1);
 		}

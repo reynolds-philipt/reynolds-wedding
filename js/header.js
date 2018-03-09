4b5d610974328479b96f4aafd61380039f8112ac
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-08 20:25:34 -0500

    show/hide header guests

--- js/header.js
+++ js/header.js
@@ -4,20 +4,18 @@ function header() {
 
   var user_saved = self.load_local('user');
   if (user_saved) {
+    if (window.user.number_of_guests && window.user.number_of_guests >= 1) {
+      show_header_confirmed_users();
+    } else {
+      hide_header_confirmed_users();
+    }
     header_user_login.innerHTML = window.user.first_name + ' ' + window.user.last_name + ' (Sign out)';
     header_user_login.addEventListener('click', function() {
       self.remove_local('user');
       render();
     });
   } else {
-    var rsvp_header = document.getElementById('header_button_rsvp');
-    rsvp_header.style.display = 'none';
-    var venue_header = document.getElementById('header_button_venue');
-    venue_header.style.display = 'none';
-    var hotel_header = document.getElementById('header_button_hotel');
-    hotel_header.style.display = 'none';
-    // var invite_header = document.getElementById('header_button_invite');
-    // invite_header.style.display = 'none';
+    hide_header_confirmed_users();
   }
 
   var pathname = window.location.pathname;
@@ -30,3 +28,46 @@ function header() {
   }
 }
 
+function hide_header_confirmed_users() {
+  var rsvp_header = document.getElementById('header_button_rsvp');
+  rsvp_header.style.display = 'none';
+  var rsvp_divider = document.getElementById('header_button_divider_rsvp');
+  rsvp_divider.style.display = 'none';
+
+  var venue_header = document.getElementById('header_button_venue');
+  venue_header.style.display = 'none';
+  var venue_divider = document.getElementById('header_button_divider_venue');
+  venue_divider.style.display = 'none';
+
+  var hotel_header = document.getElementById('header_button_hotel');
+  hotel_header.style.display = 'none';
+  var hotel_divider = document.getElementById('header_button_divider_hotel');
+  hotel_divider.style.display = 'none';
+
+  // var invite_header = document.getElementById('header_button_invite');
+  // invite_header.style.display = 'none';
+  // var invite_divider = document.getElementById('header_button_divider_invite');
+  // invite_divider.style.display = 'none';
+}
+
+function show_header_confirmed_users() {
+  var rsvp_header = document.getElementById('header_button_rsvp');
+  rsvp_header.style.display = '';
+  var rsvp_divider = document.getElementById('header_button_divider_rsvp');
+  rsvp_divider.style.display = '';
+
+  var venue_header = document.getElementById('header_button_venue');
+  venue_header.style.display = '';
+  var venue_divider = document.getElementById('header_button_divider_venue');
+  venue_divider.style.display = '';
+
+  var hotel_header = document.getElementById('header_button_hotel');
+  hotel_header.style.display = '';
+  var hotel_divider = document.getElementById('header_button_divider_hotel');
+  hotel_divider.style.display = '';
+
+  // var invite_header = document.getElementById('header_button_invite');
+  // invite_header.style.display = '';
+  // var invite_divider = document.getElementById('header_button_divider_invite');
+  // invite_divider.style.display = '';
+}

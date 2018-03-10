b13e905e227b293e0b9df1bc966202f39d1dcc93
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-10 00:40:57 -0500

    initmap

--- js/venue.js
+++ js/venue.js
@@ -1,3 +1,15 @@
 function venue() {
 	document.getElementById('header_button_venue').style.backgroundColor('#42eef4');
 }
+
+function initMap() {
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}

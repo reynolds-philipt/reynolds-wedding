2c884a14082b5c117ee96fbca2622318445ba79c
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-10 12:37:09 -0500

    backwards guest set up

--- js/login.js
+++ js/login.js
@@ -159,8 +159,9 @@ function welcome(returning) {
 	user_submit.style.display = 'none';
 	var guest_set_up = (window.user.number_of_guests && +window.user.number_of_guests > 0);
 	email_p.innerHTML = "Welcome " + (returning ? " back " : "") + window.user.first_name + "!" +
-		(guest_set_up ? "  We will send out an email<br>invitation for you once your account is set up in the next few days." :
-		 "  Please go to the RSVP page to let us know if you will be able to join us in celebrating.");
+		(guest_set_up ?
+		 "  Please go to the RSVP page to let us know if you will be able to join us in celebrating." :
+		 "  We will send out an email<br>invitation for you once your account is set up in the next few days.");
 
 	var header_user_login = document.getElementById('header_user_login_out');
 	header_user_login.innerHTML = window.user.first_name + ' ' + window.user.last_name + ' (Sign out)';

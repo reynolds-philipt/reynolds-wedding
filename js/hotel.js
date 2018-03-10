a7ca9edfd598182918285038e043a7ef8c8d82f4
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-10 01:19:50 -0500

    init map

--- js/hotel.js
+++ js/hotel.js
@@ -1,3 +1,15 @@
 function hotel() {
   
 }
+
+function initMap() {
+	var uluru = {lat: 40.0517, lng: -85.6506};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 10,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}

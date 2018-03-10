ff07e9297ee26ee0432216b92119bb24eb047ec7
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-10 00:42:47 -0500

    new location

--- js/venue.js
+++ js/venue.js
@@ -3,7 +3,7 @@ function venue() {
 }
 
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+	var uluru = {lat: 39.9769, lng: 85.7527};
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
 		center: uluru

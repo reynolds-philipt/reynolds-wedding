31dd0e0662c4bf9564792b645f782988f16562f3
reynolds-philipt <reynolds.philipt@gmail.com>
2018-03-12 21:28:16 -0400

    update local user

--- js/rsvp.js
+++ js/rsvp.js
@@ -429,6 +429,7 @@ function save_user() {
 	var new_user = database.ref('guests/' + window.user.id).set(
 		current_user
 	);
+	self.save_local("user", window.user);
 	
 	var rsvp_inputs_divs = document.getElementById('rsvp_inputs_divs');
 	rsvp_inputs_divs.innerHTML = "";
